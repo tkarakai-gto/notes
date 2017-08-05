e06109a5df345abf5406c68a28b7482e88a36913
tkarakai <tkarakai@gmail.com>
2017-08-04 19:53:06 -0500

    Now using Unix LF (instead of CRLF)

--- docker/redis-cluster/sentinel/sentinel-entrypoint.sh
+++ docker/redis-cluster/sentinel/sentinel-entrypoint.sh
@@ -6,4 +6,5 @@ sed -i "s/\$SENTINEL_QUORUM/$SENTINEL_QUORUM/g" /etc/redis/sentinel.conf
 sed -i "s/\$SENTINEL_DOWN_AFTER/$SENTINEL_DOWN_AFTER/g" /etc/redis/sentinel.conf
 sed -i "s/\$SENTINEL_FAILOVER/$SENTINEL_FAILOVER/g" /etc/redis/sentinel.conf
 
+#
 redis-server /etc/redis/sentinel.conf --sentinel
